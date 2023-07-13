9ec7d7d2f0f2eed692584b741015231e7c079930
ArmandBGarcia <xdarcx@hotmail.es>
2023-07-13 11:53:36 -0600

    first commitC

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card } from "./components/card";
+
+import "./App.css";
+import { Counter } from "./components/Counter";
+
+function App() {
+  return (
+    <div>
+      <Card />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Cards = () => {
+  return (
+    <div>
+      <h2>This is the data:</h2>
+    </div>
+  );
+};
